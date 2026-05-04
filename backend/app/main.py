0af25d76cf708f5451cbfcec635bdfc07e44131d
Marco Sabatini <marsab04188@stud.noroff.no>
2026-05-04 13:21:03 +0200

    implementing global handlers for errors, for validation, sqlalchemy error and other errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 from app.routes import crimes
 from app.models.crime import Crime  # noqa: F401 — registers model with Base.metadata
 from app.database.database import engine, Base
+
 
 app = FastAPI()
 
@@ -10,6 +14,33 @@ app.include_router(crimes.router, prefix="/crimes")
 
 Base.metadata.create_all(bind=engine)
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={
+            "detail": "Validation error",
+            "errors": exc.errors()
+        },
+    )
+
+@app.exception_handler(SQLAlchemyError)
+async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "detail": "Database error",
+        },
+    )
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request:Request , exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content= {
+            "detail": "Internal Server Error"
+        }
+    )
 
 @app.get("/health")
 def health():
